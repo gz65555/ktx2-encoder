2f816f246907f12e19b6b5a2c95f230fd0a5bead
song.hu <song.hu@mujin.co.jp>
2026-07-17 14:49:24 +0900

    chore: upgrade dependencies batch 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "example", "index.html"),
-        preview: resolve(__dirname, "example", "preview.html")
-      }
-    }
-  },
   test: {}
 });
